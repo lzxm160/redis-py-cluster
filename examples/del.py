05e86c5754d6350b3b99d815f5c5154d8149f87e
lzxm160 <lzxm160@sina.com>
2016-12-26 15:15:45 +0800

    del

--- examples/del.py
+++ examples/del.py
@@ -7,8 +7,21 @@ rc = RedisCluster(startup_nodes=startup_nodes, max_connections=32, decode_respon
 # rc.set("foo", "bar")
 # print(rc.get("{{*}_test_flow_number}:id"))
 # rc.delete("{{*}_test_flow_number}:id")
-# 1月1日早上4点
+# 1月1日23:59分
 # 59 23 1 1 * command line
+# bak data
+shutil.copy2('/usr/local/redis/data/appendonly-6380.aof', '/usr/local/redis/data/appendonly-6380-2016.12.31bak.aof')
+shutil.copy2('/usr/local/redis/data/appendonly-6381.aof', '/usr/local/redis/data/appendonly-6381-2016.12.31bak.aof')
+shutil.copy2('/usr/local/redis/data/appendonly-6382.aof', '/usr/local/redis/data/appendonly-6382-2016.12.31bak.aof')
+shutil.copy2('/usr/local/redis/data/appendonly-7380.aof', '/usr/local/redis/data/appendonly-7380-2016.12.31bak.aof')
+shutil.copy2('/usr/local/redis/data/appendonly-7381.aof', '/usr/local/redis/data/appendonly-7381-2016.12.31bak.aof')
+shutil.copy2('/usr/local/redis/data/appendonly-7382.aof', '/usr/local/redis/data/appendonly-7382-2016.12.31bak.aof')
+shutil.copy2('/usr/local/redis/data/dump-6380.rdb', '/usr/local/redis/data/dump-6380-2016.12.31bak.rdb')
+shutil.copy2('/usr/local/redis/data/dump-6381.rdb', '/usr/local/redis/data/dump-6381-2016.12.31bak.rdb')
+shutil.copy2('/usr/local/redis/data/dump-6382.rdb', '/usr/local/redis/data/dump-6382-2016.12.31bak.rdb')
+shutil.copy2('/usr/local/redis/data/dump-7380.rdb', '/usr/local/redis/data/dump-7380-2016.12.31bak.rdb')
+shutil.copy2('/usr/local/redis/data/dump-7381.rdb', '/usr/local/redis/data/dump-7381-2016.12.31bak.rdb')
+shutil.copy2('/usr/local/redis/data/dump-7382.rdb', '/usr/local/redis/data/dump-7382-2016.12.31bak.rdb')
 keys=set(rc.keys(pattern='{*_test_flow_number}:id'))
 for i in keys:
 	print(rc.get(i))
