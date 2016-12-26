76856d762c12b9ddbc2ba577f13c3af909df323d
lzxm160 <lzxm160@sina.com>
2016-12-26 13:16:40 +0800

    del

--- examples/del.py
+++ examples/del.py
@@ -6,7 +6,7 @@ r = RedisCluster(startup_nodes=startup_nodes, max_connections=32, decode_respons
 # rc.set("foo", "bar")
 # rc.set("foo", "bar")
 print(rc.get("{test3_test_flow_number}:id"))
-rc.del("{test3_test_flow_number}:id")
+# rc.del("{test3_test_flow_number}:id")
 # for i in xrange(1000000):
 #     d = str(i)
 #     r.set(d, d)
