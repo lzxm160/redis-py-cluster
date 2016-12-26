83e2c804adf8ec78c086f7752f90de406c0bd88b
lzxm160 <lzxm160@sina.com>
2016-12-26 15:53:28 +0800

    del

--- examples/del.py
+++ examples/del.py
@@ -11,27 +11,11 @@ rc = RedisCluster(startup_nodes=startup_nodes, max_connections=32, decode_respon
 # 1.1 23:59
 # 59 23 1 1 * command line
 # bak data
-port=['6380','6381','6382','7380','7381','7382']
-for i in port:
-	from='/usr/local/redis/data/appendonly-'+i+'.aof'
-	to='/usr/local/redis/data/appendonly-'+i+'-2016.12.31bak.aof'
-	try:
-		shutil.copy2(from, to)
-	except (IOError, os.error, shutil.Error), why:
-            print("Unable to remove \"%s\": %s" % (cur_path, why))
-            continue;
-for i in port:
-	from='/usr/local/redis/data/dump-'+i+'.rdb'
-	to='/usr/local/redis/data/dump-'+i+'-2016.12.31bak.rdb'
-	try:
-		shutil.copy2(from, to)
-	except (IOError, os.error, shutil.Error), why:
-            print("Unable to remove \"%s\": %s" % (cur_path, why))
-            continue;
 
 keys=set(rc.keys(pattern='{*_test_flow_number}:id'))
 for i in keys:
-	print(rc.get(i))
+	print("\"%s\": %s" % (i, rc.get(i)))
+	
 	rc.delete(i)
 # print(rc.get("{test2_test_flow_number}:id"))
 # rc.delete("{test2_test_flow_number}:id")
