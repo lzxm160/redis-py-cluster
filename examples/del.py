d2369f057a364601e7e903f6a106c8610d62261e
lzxm160 <lzxm160@sina.com>
2016-12-26 17:10:23 +0800

    del

--- examples/del.py
+++ examples/del.py
@@ -1,7 +1,8 @@
 from redis._compat import xrange
 from rediscluster import RedisCluster
 import shutil
-
+import datetime
+import os
 startup_nodes = [{"host": "127.0.0.1", "port": 6380}]
 rc = RedisCluster(startup_nodes=startup_nodes, max_connections=32, decode_responses=True)
 # rc.set("foo", "bar")
@@ -11,12 +12,17 @@ rc = RedisCluster(startup_nodes=startup_nodes, max_connections=32, decode_respon
 # 1.1 23:59
 # 59 23 1 1 * command line
 # bak data
-
+file=os.path.split(os.path.realpath(__file__))[0]+'/flow_no.txt'
+f=open(file,"a")
+f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+f.write('---------------------\r\n')
 keys=set(rc.keys(pattern='{*_test_flow_number}:id'))
 for i in keys:
-	print("\"%s\": %s" % (i, rc.get(i)))
-	
+	str=i+':'+ rc.get(i)+'\r\n'
+	print(str)
+	f.write(str)
 	rc.delete(i)
+f.close()
 # print(rc.get("{test2_test_flow_number}:id"))
 # rc.delete("{test2_test_flow_number}:id")
 # print(rc.get("{test1_test_flow_number}:id"))
