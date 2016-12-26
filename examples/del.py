9b7309942aee35df851d5e2b7fc78f7123fe7540
lzxm160 <lzxm160@sina.com>
2016-12-26 15:02:08 +0800

    del

--- examples/del.py
+++ examples/del.py
@@ -7,7 +7,8 @@ rc = RedisCluster(startup_nodes=startup_nodes, max_connections=32, decode_respon
 # rc.set("foo", "bar")
 # print(rc.get("{{*}_test_flow_number}:id"))
 # rc.delete("{{*}_test_flow_number}:id")
-
+# 1月1日早上4点
+# 59 23 1 1 * command line
 keys=set(rc.keys(pattern='{*_test_flow_number}:id'))
 for i in keys:
 	print(rc.get(i))
