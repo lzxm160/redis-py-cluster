1d9351be80d4859f7a6d58f843c2bd14d4183ea4
lzxm160 <lzxm160@sina.com>
2017-02-03 15:20:16 +0800

    sdiff

--- examples/incr-test-writer.py
+++ examples/incr-test-writer.py
@@ -2,7 +2,7 @@ from redis._compat import xrange
 from rediscluster import RedisCluster
 
 startup_nodes = [{"host": "127.0.0.1", "port": 6380}]
-r = RedisCluster(startup_nodes=startup_nodes, max_connections=32, decode_responses=True)
+rc = RedisCluster(startup_nodes=startup_nodes, max_connections=32, decode_responses=True)
 #rc.set("foo", "bar")
 
 #print(rc.get("{test3_test_flow_number}:id"))
